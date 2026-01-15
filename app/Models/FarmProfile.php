f6a7dcd1889d0ecd813be8cf9c15070388767c73
Ferjose007 <167478921+Ferjose007@users.noreply.github.com>
2026-01-14 20:38:33 -0500

    Configuración model FarmProfile

--- app/Models/FarmProfile.php
+++ app/Models/FarmProfile.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class FarmProfile extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'farm_name',
+        'location_lat',
+        'location_lng',
+        'bio',
+        'soil_type',
+        'whatsapp_number'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function weatherLogs()
+    {
+        return $this->hasMany(WeatherLog::class);
+    }
 }
