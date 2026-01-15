5e1fb1fb6705e08bfb4e7781b60c4df764585e26
Ferjose007 <167478921+Ferjose007@users.noreply.github.com>
2026-01-14 20:50:13 -0500

    Configuración model PriceBreakdowns

--- app/Models/PriceBreakdown.php
+++ app/Models/PriceBreakdown.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PriceBreakdown extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'farmer_earning',
+        'plataform_fee',
+        'logistics_cost',
+        'taxes'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
