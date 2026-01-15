71836b924ff73dcf4348a980bb35b5d7f0e6e128
Ferjose007 <167478921+Ferjose007@users.noreply.github.com>
2026-01-14 20:20:53 -0500

    Cambios en user_table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('admin', 'farmer', 'buyer');
+            $table->enum('role', ['admin', 'farmer', 'buyer'])->default('buyer');
             $table->rememberToken();
             $table->timestamps();
         });
